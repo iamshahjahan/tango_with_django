cdd2c57923eec47e14b7581e246087cf1d0fa75c
Mehrab Alam <mehrab.alam@cube26.com>
2016-03-24 18:25:09 +0530

    some changes made

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,12 +3,12 @@ from rango import views
 from django.conf import settings
 
 urlpatterns = patterns('',url(r'^$',views.index,name='index')
-						 ,url(r'^about/$',views.about,name='about')
-						 ,url(r'^add_category/$',views.add_category,name='add_category')
-						 ,url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.category, name ='category')
-						 ,url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',views.add_page, name ='add_page')
+                         ,url(r'^about/$',views.about,name='about')
+                         ,url(r'^add_category/$',views.add_category,name='add_category')
+                         ,url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.category, name ='category')
+                         ,url(r'^category/(?P<category_name_slug>\w+)/add_page/$',views.add_page, name ='add_page')
 
-						 )
+                         )
 # urlpatterns = patterns('',)
 if settings.DEBUG:
     urlpatterns += patterns(
@@ -16,3 +16,8 @@ if settings.DEBUG:
         (r'^media/(?P<path>.*)',
         'serve',
         {'document_root': settings.MEDIA_ROOT}), )
+
+
+
+
+
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,93 +1,99 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from rango.forms import CategoryForm,PageForm
-from rango.models import Category,Page
+from django.http import HttpResponse, HttpResponseRedirect
+from rango.forms import CategoryForm, PageForm
+from rango.models import Category, Page
 
 def index(request):
 # sending the data from category model to the server:
 
-	context_catogaries = Category.objects.order_by('-likes')[:18]
-	context_pages = Page.objects.order_by('-views')[:18]
+    context_catogaries = Category.objects.order_by('-likes')[:18]
+    context_pages = Page.objects.order_by('-views')[:18]
 
-	context_dict = {'categories' : context_catogaries , 'pages' : context_pages}
+    context_dict = {'categories' : context_catogaries , 'pages' : context_pages}
 
-	return render(request,'rango/index.html',context_dict)
+    return render(request,'rango/index.html',context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page. Click <a href='../'> here</a>")
+    return HttpResponse("Rango says here is the about page. Click <a href='../'> here</a>")
  # working here for the category page
 
 def category(request,category_name_slug):
- 	context_dict = []
+    context_dict = []
 
- 	try:
- 		c = Category.objects.get(slug = category_name_slug)
+    try:
+        c = Category.objects.get(slug = category_name_slug)
 
 
-		# context_dict['category_name'] = c.name
+        # context_dict['category_name'] = c.name
 
-		pages = Page.objects.filter(category = c)
+        pages = Page.objects.filter(category = c)
 
-		for page in pages:
-			print page.title
+        for page in pages:
+            print page.title
 
-		# context_dict['pages'] = pages
-		# context_dict['category'] = c
-		context_dict = {'category_name' : c.name,'pages':pages,'category':c} 
+        # context_dict['pages'] = pages
+        # context_dict['category'] = c
+        context_dict = {'category_name' : c.name,'pages':pages,'category':c} 
 
-	except Exception, e:
-		print e
-		pass
+    except Exception, e:
+        print e
+        pass
 
-	return render(request,'rango/category.html',context_dict)
+    return render(request,'rango/category.html',context_dict)
 
 # defining form data
 def add_category(request):
-	# check for post method
+    # check for post method
 
-	if request.method == 'POST':
-		# means you submitted the form.
-		form = CategoryForm(request.POST)
+    if request.method == 'POST':
+        # means you submitted the form.
+        form = CategoryForm(request.POST)
 
-		if form.is_valid():
-			form.save(commit = True)
+        if form.is_valid():
+            form.save(commit = True)
 
-			return index(request)
-		else:
-			print form.errors
+            return index(request)
+        else:
+            print form.errors
 
-	else:
-		form = CategoryForm()
+    else:
+        form = CategoryForm()
 
-	return render(request,'rango/add_category.html',{'form' : form})
+    return render(request,'rango/add_category.html',{'form' : form})
 
 # adding page here
-def add_page(request,category_name_slug):
-	# check for post method
+def add_page(request,category_name_slug=None):
+    # check for post method
+    print category_name_slug
+    try:
+        cat = Category.objects.get(slug = category_name_slug)
+    except Exception, e:
+        cat = None
 
-	try:
-		cat = Category.objects.get(slug = category_name_slug)
-	except Exception, e:
-		cat = None
+
+    if request.method == 'POST':
+        print "sjhdfkjsdhkfhjs"
+        # means you submitted the form.
+        form = PageForm(request.POST)
+        a = form.is_valid()
+        if a:
+            if cat:
+                page = form.save(commit = False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                print "sdjhfjshk"
+                return HttpResponseRedirect('/rango/category/' + category_name_slug + '/')
+        else:
+            print form.errors
+
+    else:
+        form = PageForm()
+
+    context_dict = {'form' : form,'category' : cat, 'slug': category_name_slug}
+
+    return render(request,'rango/add_page.html', context_dict)
 
 
-	if request.method == 'POST':
-		# means you submitted the form.
-		form = PageForm(request.POST)
 
-		if form.is_valid():
-			if cat:
-				page = form.save(commit = False)
-				page.category = cat
-				page.views = 0
-				page.save
-				return category(request,category_name_slug)
-		else:
-			print form.errors
 
-	else:
-		form = PageForm()
-
-	context_dict = {'form' : form,'category' : cat}
-
-	return render(request,'rango/category/'+ cat.slug +'/add_page.html',context_dict)
